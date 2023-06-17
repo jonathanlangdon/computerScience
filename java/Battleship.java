f6d96f3e394c90e93863a3c4dc8bc531bb65162d
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-06-17 15:43:43 -0400

    fireOnTarget method working fully

--- java/Battleship.java
+++ java/Battleship.java
@@ -8,37 +8,39 @@ public class Battleship {
 		char[][] homeSea2 = new char[5][5];
 		// int [][] shipsArr1 = getHomeShips(1, homeSea1);
 		// int [][] shipsArr2 = getHomeShips(2, homeSea2);
-		int[][] shipsArr1 = {{1,1},{2,2},{3,3},{4,4},{0,0}};
-		int[][] shipsArr2 = {{1,1},{2,2},{3,3},{4,4},{0,0}};
-		playGame();
+		int[][] shipsArr1 = {{1,1},{1,2},{1,3},{1,4},{1,0}};
+		int[][] shipsArr2 = {{2,1},{2,2},{2,3},{2,4},{2,0}};
+		playGame(shipsArr1, shipsArr2);
 	}
 	
-	private static void playGame() {
-		Scanner sc = new Scanner(System.in);
+	private static void playGame(int[][] shipsArr1, int[][] shipsArr2) {
 		char[][] enemySea1 = new char[5][5];
 		char[][] enemySea2 = new char[5][5];
-		int hits1 = 0;
-		int hits2 = 0;
-		while(hits1 < 5 && hits2 < 5) {
-			int activePlayer = 1;
-			fireOnTarget(hits2, activePlayer, enemySea2);
+		int hitsOn1 = 0;
+		int hitsOn2 = 0;
+		while(hitsOn1 < 5 && hitsOn2 < 5) {
+			// fireOnTarget(hitsOn2, 1, enemySea2, shipsArr2);
+			hitsOn1 = fireOnTarget(hitsOn1, 2, enemySea1, shipsArr1);
+			System.out.printf("Player 1 has %d hits and Player 2 has %d hits%n%n", hitsOn2, hitsOn1);
 		}
 		int winner = 0;
-		if (hits1 == 5) winner = 1;
-		else winner = 2;
+		if (hitsOn1 == 5) winner = 2;
+		else winner = 1;
 		System.out.printf("PLAYER %d WINS! YOU SUNK ALL OF YOUR OPPONENT’S SHIPS!%n", winner);
 	}
 
-	private static int[][] fireOnTarget(int hits, int activePlayer, char[][] enemySea) {
+	private static int fireOnTarget(int hits, int activePlayer, char[][] enemySea, int[][] shipsArr) {
+		Scanner sc = new Scanner(System.in);
 		System.out.printf("Player %d, enter hit row/column:", activePlayer);
 		Boolean validInput = false;
+		int num1 = 0;
+		int num2 = 0;
 		do {
 			try {
-				System.out.printf("Enter ship %d location:%n", i+1);
 				String input = sc.nextLine();
 				String[] inputArr = input.split(" ");
-				int num1 = Integer.parseInt(inputArr[0]);
-				int num2 = Integer.parseInt(inputArr[1]);
+				num1 = Integer.parseInt(inputArr[0]);
+				num2 = Integer.parseInt(inputArr[1]);
 				if (num1 > 4 || num2 > 4) throw new Exception();
 				if(enemySea[num1][num2] == 'X' || enemySea[num1][num2] == 'O') {
 					System.out.println("You already fired on this spot. Choose different coordinates.");
@@ -49,6 +51,22 @@ public class Battleship {
 				System.out.println("Invalid coordinates. Choose different coordinates.");
 			}
 		} while (!validInput);
+		int targetPlayer = activePlayer == 1 ? 2 : 1;
+		boolean targetIsHit = false;
+		for (int[] ship : shipsArr) {
+			if (ship[0] == num1 && ship[1] == num2) {
+				targetIsHit = true;
+			}
+		}
+		if (targetIsHit) {
+				System.out.printf("PLAYER %d HIT PLAYER %d’s SHIP!%n", activePlayer, targetPlayer);
+				enemySea[num1][num2] = 'X';
+				hits += 1;
+			} else {
+				System.out.printf("PLAYER %d MISSED!%n", activePlayer);
+				enemySea[num1][num2] = 'O';
+			}
+		return hits;
 	}
 	
 	private static int[][] getHomeShips(int playerNum, char[][] homeSea) {
