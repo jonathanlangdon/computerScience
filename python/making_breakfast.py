1707c20ba82e1297fab3b41032fdd4b51cdf834c
Jonathan Langdon <langdon@Langdon-MBP.local>
2023-07-11 10:58:12 -0400

    # Making Breakfast

---
+++ python/making_breakfast.py
@@ -0,0 +1,21 @@
+# Making Breakfast
+
+egg = False
+milk = True
+butter = False
+flour = True
+
+if egg and milk and butter and flour:
+    print("pancakes")
+elif egg and milk and butter:
+    print("omelette")
+elif egg and milk:
+    print("custard")
+elif egg:
+    print("poached eggs")
+else:
+    print("Go to the store!")
+
+"""
+more practice with conditionals
+"""
