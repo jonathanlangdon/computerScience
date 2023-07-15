a74a23033b145a0ddd6fcc827e79785fa1cb1044
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-15 09:55:57 -0400

    # add all integers up to mystery integer (works for negatives too)

---
+++ python/sum_loop.py
@@ -0,0 +1,16 @@
+# add all integers up to mystery integer (works for negatives too)
+
+mystery_int = -3
+
+mysum = 0
+operator = ""
+
+if mystery_int > 0:
+    operator = "+"
+else:
+    operator = "-"
+
+for i in range(0, eval(f"{mystery_int}{operator}1"), eval(f"{operator}1")):
+    mysum += i
+
+print(mysum)
