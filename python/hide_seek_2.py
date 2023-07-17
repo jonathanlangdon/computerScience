8fabf0e6979669565bd93990fe36c2b727c6ae98
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-17 09:45:41 -0400

    # count to a inputed number and say here i come

---
+++ python/hide_seek_2.py
@@ -0,0 +1,10 @@
+# count to a inputed number and say here i come
+
+
+def hide_and_seek(high):
+    for num in range(1, high + 1):
+        print(num)
+    print("Ready or not, here I come!")
+
+
+hide_and_seek(23)
