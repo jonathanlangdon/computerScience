e08132c2db3c52da1bc8d70ddd051842628583cc
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-17 09:45:53 -0400

    # practice writing  function that returns a string with parameters

---
+++ python/my_ta.py
@@ -0,0 +1,5 @@
+# practice writing  function that returns a string with parameters
+
+
+def my_TAs(first_TA, second_TA, third_TA):
+    return f"{first_TA}, {second_TA}, and {third_TA} are awesome!"
