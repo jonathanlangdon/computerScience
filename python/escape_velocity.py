0fbd1a3cb63086c0f66bb4f6a69d43a8d510719b
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-20 08:40:57 -0400

    # convert escape velocity to various units

---
+++ python/escape_velocity.py
@@ -0,0 +1,10 @@
+# convert escape velocity to various units
+
+
+def escape_velocity(escape_velocity, in_kilometers=True, in_seconds=True):
+    if not in_kilometers:
+        escape_velocity *= 0.62
+    if not in_seconds:
+        escape_velocity *= 3600
+
+    return round(escape_velocity, 2)
