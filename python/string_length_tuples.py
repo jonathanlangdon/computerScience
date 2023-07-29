a05a33f04fac394cd3524d054bcbf5fe554e22ff
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-07-29 07:27:01 -0400

    # avergae all the lines of a file

--- python/string_length_tuples.py
+++
@@ -1,5 +0,0 @@
-# return a tuple that
-
-
-def string_length(text):
-    return (text, len(text))
--- python/string_to_ordinals.py
+++
@@ -1,8 +0,0 @@
-# convert char in a string to its ordinal numbers
-
-
-def string_to_ordinals(text):
-    result = ""
-    for char in text:
-        result += str(ord(char))
-    return result
