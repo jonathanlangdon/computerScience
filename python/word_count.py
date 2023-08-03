4f4ae1364e94bdf09ca074d79149f4f191749439
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-02 23:05:29 -0400

    # return dictionary as a long string after being sorted

--- python/word_count.py
+++
@@ -1,8 +0,0 @@
-# count the number of words with error handling
-
-
-def word_count(my_string):
-    try:
-        return my_string.count(" ") + 1
-    except:
-        return "Not a string"
--- python/word_count2.py
+++
@@ -1,8 +0,0 @@
-# count the number of words with error handling
-
-
-def word_count(my_string):
-    try:
-        return len(my_string.split())
-    except:
-        return "Not a string"
