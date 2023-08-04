8bdb332828ed1b05bedf565a4d37e07e53587d4f
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-03 22:43:01 -0400

    # translate words to french based on dictionary

--- python/bueller.py
+++
@@ -1,9 +0,0 @@
-# return a list of present students - dictionary practice
-
-
-def students_present(a_dict):
-    present_list = []
-    for name, value in a_dict.items():
-        if value == "Here" or value == "Present":
-            present_list.append(name)
-    return present_list
--- python/simple_dictionary.py
+++
@@ -1,8 +0,0 @@
-# lookup a key to see if is in a dictionary
-
-
-def safe_lookup(adict, akey):
-    if akey in adict:
-        return adict[akey]
-    else:
-        return None
