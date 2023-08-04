f1df44d8f8bb3012ab78e76be2f6ff85dd92a4c6
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-03 22:43:04 -0400

    # return most active person in a dictionary

---
+++ python/most_active.py
@@ -0,0 +1,9 @@
+# return most active person in a dictionary
+
+
+def most_active(adict):
+    highest = ("", 0)
+    for name, days in adict.items():
+        if days > highest[1]:
+            highest = (name, days)
+    return highest[0]
