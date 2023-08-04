13022cd93f83bbb06ea12399209e74316dbb6bfc
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-04 19:53:34 -0400

    # check student test vs answer key (both are dictionaries)

--- python/most_active.py
+++
@@ -1,9 +0,0 @@
-# return most active person in a dictionary
-
-
-def most_active(adict):
-    highest = ("", 0)
-    for name, days in adict.items():
-        if days > highest[1]:
-            highest = (name, days)
-    return highest[0]
--- python/multiply_by_index.py
+++
@@ -1,7 +0,0 @@
-# multiple every item in a list by its index
-
-
-def multiply_by_index(alist):
-    for i in range(0, len(alist)):
-        alist[i] *= i
-    return alist
