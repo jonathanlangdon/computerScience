6100316bc8ea797d89875bce2f65a560c4e406f3
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-03 22:43:16 -0400

    # write output file from input file.. multiplying

---
+++ python/multiply_file_by_index.py
@@ -0,0 +1,11 @@
+# write output file from input file.. multiplying
+
+
+def multiply_file_by_index(output_file, input_file):
+    read_file = open(input_file, "r")
+    write_file = open(output_file, "w")
+    lines = read_file.readlines()
+    for i in range(1, len(lines) + 1):
+        print(int(lines[i - 1]) * i, file=write_file)
+    read_file.close()
+    write_file.close()
