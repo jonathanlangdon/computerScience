0cbd1f6cc431193c9aa6652f8f85328404af8d95
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-09 18:23:25 -0400

    # example of how zero based indexing works with a for each loop

--- python/3Looped.py
+++
@@ -1,4 +0,0 @@
-# print string three times with this loop
-
-for a in [10, 9, 8]:
-    print("Looped!")
--- python/after_second.py
+++
@@ -1,7 +0,0 @@
-# return all text after 2nd instance of a search item
-
-
-def after_second(search_in, search_for):
-    first_index = search_in.find(search_for)
-    second_index = search_in.find(search_for, first_index + 1)
-    return search_in[(second_index + len(search_for)) :]
