1543633ed572f6cb105485493237a56c87d95e69
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-16 22:07:20 -0400

    # determine whether you want to buy an item using conditional logic

---
+++ python/wish_list.py
@@ -0,0 +1,10 @@
+# determine whether you want to buy an item using conditional logic
+
+
+def wish_list(alist, item, cost, budget):
+    if item in alist and cost <= budget:
+        return f"You should buy a {item}!"
+    if item in alist and cost > budget:
+        return f"You should save up for a {item}!"
+    if item not in alist:
+        return f"You probably don't want to buy a {item}."
