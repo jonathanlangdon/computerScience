659b3daab5b6305777a4537e9adaad78878917ab
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-18 10:30:02 -0400

    // adding constructor chaining

--- java/Fly.java
+++ java/Fly.java
@@ -1,6 +1,29 @@
-// starting template for Fly class
+// adding constructor chaining
 
 public class Fly {
+  //instance variables
+  private double mass;
+  private double speed;
+
+  //constants
+  public static final double DEFAULT_MASS = 5;
+  public static final double DEFAULT_SPEED = 10;
+
+
+  //constructors
+  public Fly() {
+    this(DEFAULT_MASS);
+  }
+
+  public Fly(double initMass) {
+    this(initMass, DEFAULT_SPEED);
+  }
+
+  public Fly(double initMass, double initSpeed) {
+    mass = initMass;
+    speed = initSpeed;
+  }
+
   public static void main() {
 
   }
