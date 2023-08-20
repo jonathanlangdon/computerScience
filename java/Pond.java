782f6c57cbe664565f7d49daf5ada67515191275
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-19 23:17:39 -0400

    // new frog objects created

---
+++ java/Pond.java
@@ -0,0 +1,10 @@
+// new frog objects created
+
+public class Pond {
+  public static void main() {
+    Frog frogPeepo = new Frog("Peepo");
+    Frog frogPepe = new Frog("Pepe", 10, 15);
+    Frog frogPeepaw = new Frog("Peepaw", 4.6, 5);
+    Frog frogPudu = new Frog("Pudu");
+  }
+}
