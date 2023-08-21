93f7c8df7647ea7d79fb62206d02c611133265bf
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-08-20 21:28:19 -0400

    // adding get method for speed

---
+++ java/Fly.java
@@ -0,0 +1,39 @@
+// adding get method for speed
+
+public class Fly {
+  //instance variables
+  private double mass;
+  private double speed;
+
+  //constants
+  public static final double DEFAULT_MASS = 5;
+  public static final double DEFAULT_SPEED = 10;
+
+
+  //constructors
+  public Fly() {
+    this(DEFAULT_MASS);
+  }
+
+  public Fly(double mass) {
+    this(mass, DEFAULT_SPEED);
+  }
+
+  public Fly(double mass, double speed) {
+    this.mass = mass;
+    this.speed = speed;
+  }
+
+  //methods
+  public double getMass() {
+    return this.mass;
+  }
+
+  public double getSpeed() {
+    return this.speed;
+  }
+
+  public static void main() {
+
+  }
+}
