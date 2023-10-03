f162f3d4bf4c6cdbcdb3725a730b9fa0c898958c
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-03 07:48:32 -0400

    # How many different boys' names are there that begin with the letter Z?

--- python/baby_names01.py
+++
@@ -1,7 +0,0 @@
-# get how many names are in database
-
-names_file = open("baby_names.csv", "r")
-# in actual database, use the actual filename
-
-print(len(names_file.readlines()))
-names_file.close()
