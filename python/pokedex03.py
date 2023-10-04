68eb9da7bc93096e3bc0581bf57d27f740663bb2
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-10-04 17:59:16 -0400

    # What Pokemon has the highest HP statistic?

---
+++ python/pokedex03.py
@@ -0,0 +1,58 @@
+# What Pokemon has the highest HP statistic?
+
+pokedex = open("pokedex.csv", "r")
+# above is for testing, but use actual data source
+
+header = next(pokedex)
+
+
+class Pokemon:
+    def __init__(
+        self,
+        num,
+        name,
+        type1,
+        type2,
+        hp,
+        attack,
+        defense,
+        special_atk,
+        special_def,
+        speed,
+        generation,
+        legendary,
+        mega,
+    ):
+        self.num = int(num)
+        self.name = name
+        self.type1 = type1
+        self.type2 = type2
+        self.hp = int(hp)
+        self.attack = int(attack)
+        self.defense = int(defense)
+        self.special_atk = int(special_atk)
+        self.special_def = int(special_def)
+        self.speed = int(speed)
+        self.generation = int(generation)
+        self.legendary = legendary.capitalize()
+        self.mega = mega.capitalize()
+
+    def __str__(self):
+        return f"Pokemon{self.num}: {self.name}"
+
+
+pokemon_list = []
+for line in pokedex:
+    stats = line.strip().split(",")
+    pokemon_list.append(Pokemon(*stats))
+pokedex.close()
+
+highest_hp = (None, 0)
+for pokemon in pokemon_list:
+    if pokemon.hp > highest_hp[1]:
+        highest_hp = pokemon.name, pokemon.hp
+
+print(highest_hp)
+
+# line 1: 'Number,Name,Type1,Type2,HP,Attack,Defense,SpecialAtk,SpecialDef,Speed,Generation,Legendary,Mega\n'
+# line 2: '2,Bulbasaur,Grass,Poison,45,49,49,65,65,45,1,FALSE,FALSE\n'
